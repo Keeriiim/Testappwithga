09037aea84e36089db0f39d180a9facd0744adc3
kerim kozo <kerim@kerims-MBP.lan>
2024-02-13 14:57:11 +0100

    Edited javadocs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,38 @@
 package org.example;
 
-@SuppressWarnings("javadoc")
+/**
+ * This class represents a simple program that prints a message and adds two numbers.
+ */
 public class Main {
 
+    /** The message to be printed */
     private static String myMessage = "Hello World!";
-    private static int apples= 10;
-    private static int oranges= 20;
 
-    public static int add(int apples, int oranges) {
-        return apples + oranges;
+    /** The number of apples */
+    private static int APPLES = 10;
+
+    /** The number of oranges */
+    private static int ORANGES = 20;
+
+    /**
+     * Adds two numbers and returns the result.
+     *
+     * @param a The first number (apples)
+     * @param b The second number (oranges)
+     * @return The sum of the two numbers
+     */
+    public static int add(int a, int b) {
+        return a + b;
     }
 
+    /**
+     * The main method of the program.
+     *
+     * @param args The command-line arguments (not used in this program)
+     */
     public static void main(String[] args) {
-
-        System.out.println(String.format("%s, number %d", myMessage, add(apples, oranges)));
-
-
+        // Print the message along with the sum of apples and oranges
+        System.out.println(String.format("%s, number %d", myMessage, add(APPLES, ORANGES)));
     }
 }
+
