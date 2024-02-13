0e6f7a89f8fea011b1e3bb49fa9cbd04a636f0f7
kerim kozo <kerim@kerims-MBP.lan>
2024-02-13 14:51:31 +0100

    Edited javadocs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,19 @@
 package org.example;
 
-
+@SuppressWarnings("javadoc")
 public class Main {
 
     private static String myMessage = "Hello World!";
-    private static int number1= 10;
-    private static int number2= 20;
+    private static int apples= 10;
+    private static int oranges= 20;
 
-    public static int add(int number1, int number2) {
-        return number1 + number2;
+    public static int add(int apples, int oranges) {
+        return apples + oranges;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(String.format("%s, number %d", myMessage, add(number1, number2)));
+        System.out.println(String.format("%s, number %d", myMessage, add(apples, oranges)));
 
 
     }
